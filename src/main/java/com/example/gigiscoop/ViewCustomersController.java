a0bae18c37ba25c6f189f5af3a2da644f823f72c
Brandon Riddle <62026004+brandonriddle828@users.noreply.github.com>
2023-05-17 18:09:05 -0400

    Update ViewCustomersController.java

--- src/main/java/com/example/gigiscoop/ViewCustomersController.java
+++ src/main/java/com/example/gigiscoop/ViewCustomersController.java
@@ -34,7 +34,7 @@ public class ViewCustomersController implements Initializable
 
     String url = ("jdbc:mysql://localhost:3306/gigiscoop");
     String username = ("root");
-    String password = ("Roxysdad828!");
+    String password = ("1!");
 
     @FXML
     protected TableView<Customer> customerTable;
