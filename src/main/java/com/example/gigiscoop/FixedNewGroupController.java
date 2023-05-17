7c643a2e2a786e8c1bccf8e55b6205fb9c04d377
Brandon Riddle <62026004+brandonriddle828@users.noreply.github.com>
2023-05-17 18:05:46 -0400

    Update FixedNewGroupController.java

--- src/main/java/com/example/gigiscoop/FixedNewGroupController.java
+++ src/main/java/com/example/gigiscoop/FixedNewGroupController.java
@@ -32,7 +32,7 @@ public class FixedNewGroupController implements Initializable
 
     String url = ("jdbc:mysql://localhost:3306/gigiscoop");
     String username = ("root");
-    String password = ("Roxysdad828!");
+    String password = ("1!");
 
     @FXML
     protected ComboBox breedSelector;
